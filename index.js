fc5e98132224f64136df7eff65160b6396e57a7a
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-05-25 18:35:02 +0600

    created ema john server

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ubvegtf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const productsCollection = client.db('emaJohn').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(err => console.error(err));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Ema John simple server.');
+});
+
+app.listen(port, () => {
+    console.log(`Ema John server running on: ${port}`);
+});
